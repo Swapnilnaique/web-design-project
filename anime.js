95842aeb4b11932dddf50ae8076552b17ad3c655
Swapnil <swapnilnaique2005@gmail.com>
2022-12-13 23:12:19 +0530

    new animation on index.html (unsuccessful)

--- anime.js
+++ anime.js
@@ -40,26 +40,57 @@
 //     }
 //   });
 // });
+// function scrollTrigger(selector, options = {}){
+    // let els = document.querySelectorAll(selector)
+//     els = Array.from(els)
+//     els.forEach(el => {
+//         addObserver(el, options)
+//     })
+// }
 
-function ScrollTrigger (revealUp)
-function scrollTrigger(selector) {
-    let els = document.querySelectorAll(selector)
-    els = Array.from(els)
-    els.forEach(el => {
-      addObserver(el)
-    })
-  }
+// function addObserver(el, options){
+//     if(!('IntersectionObserver' in window)){
+//         if(options.cb){
+//             options.cb(el)
+//         }else{
+//             entry.target.classList.add('active')
+//         }
+//         return
+//     }
+//     let observer = new IntersectionObserver((entries, observer) => { //this takes a callback function which receives two arguments: the elemts list and the observer instance
+//         entries.forEach(entry => {
+//             if(entry.isIntersecting){
+//                 if(options.cb){
+//                     options.cb(el)
+//                 }else{
+//                     entry.target.classList.add('active')
+//                 }
+//                 observer.unobserve(entry.target)
+//             }
+//         })
+//     }, options)
+//     observer.observe(el)
+// }
+// // Example usages:
+// scrollTrigger('.intro-text')
 
-  function addObserver(el){
-    let observer = new IntersectionObserver((entries, observer) => { 
-      entries.forEach(entry => {
-      if(entry.isIntersecting) {
-        entry.target.classList.add('active')
-        observer.unobserve(entry.target)
-      }
+// scrollTrigger('.scroll-reveal', {
+//     rootMargin: '-200px',
+// })
+
+// scrollTrigger('.loader', {
+//     rootMargin: '-200px',
+//     cb: function(el){
+//         el.innerText = 'Loading...'
+//         setTimeout(() => {
+//             el.innerText = 'Task Complete!'
+//         }, 1000)
+//     }
+// })
+
+
+/*INTRO TEXT WITH LETTERING JS*/
+
+$(document).ready(function() {
+    $(".intro-text").lettering('words').children("span").lettering(); 
     })
-  })
-  observer.observe(el)
-}
-  // Example usage
-  scrollTrigger('.scroll-reveal')
